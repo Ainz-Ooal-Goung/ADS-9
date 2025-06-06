11eead219c3b767c9fb635f24e62f2b32257659d
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-06-06 03:27:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 #include <cstdint>
 #include "tree.h"
 
+static int64_t factorial(int n) {
+  int64_t res = 1;
+  for (int i = 2; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
+}
+
 int main() {
   std::cout << "n,total_ns1,total_ns2,total_ns_all\n";
   const int REPEATS = 10000;
